42245f51bb656db9793a4f9441fff2d3686cc8e7
jerry <brownjer3@gmail.com>
2021-05-11 10:34:34 -0700

    alter alignment of app header on /home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from './Navbar'
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Navbar from './Navbar'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Home />
+      {/* <Router>
         <>
           <Navbar />
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
         </>
-      </Router>
+      </Router> */}
+      <Footer />
     </div>
   );
 }
