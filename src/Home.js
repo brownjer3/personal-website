782b0969413cb59d4fa04b400d5b21e1f6dfee4c
jerry <brownjer3@gmail.com>
2021-05-11 10:54:59 -0700

    include about blurb container on /home

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
 export default function Home() {
     return (
         <header className="App-header">
-            <p>Hello World! My name's Jerry.</p>
-            
+            <Container>
+                <h1>Hello World! My name's Jerry.</h1>
+                <p>
+                I'm a user-focused Product Manager turned Software Engineer with experience planning and executing long-term product developement roadmaps to creating usable software
+                </p>
+            </Container>
         </header>
     )
 }
