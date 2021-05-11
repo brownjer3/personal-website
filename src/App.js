50f32f37ec3f80a9d88425a4d37bf09cee8e4f5f
jerry <brownjer3@gmail.com>
2021-05-11 09:23:11 -0700

    install react-router-dom for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Hello World! My name's Jerry.
         </p>
