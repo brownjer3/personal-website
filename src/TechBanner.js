96e2728aa440dd0ae5dd961c1ad99b1953bbb97f
jerry <brownjer3@gmail.com>
2021-05-12 10:35:49 -0700

    adjust tech icons sizing

--- src/TechBanner.js
+++ src/TechBanner.js
@@ -1,16 +1,17 @@
 import { Component } from "react";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import { techIcons } from './data/TechData'
 
 export default class TechBanner extends Component {
     makeIcons = () => {
-        return techIcons.map(icon => <i class={icon} />)
+        return techIcons.map(icon => <Col className='align-items-center'><i className={`${icon} techIcon`} /></Col>)
     }
 
     render() {
         return(
-            <Container className='border'>
+            <Container>
                 <Row className="d-flex justify-content-between align-items-center">
                     {this.makeIcons()}
                 </Row>
