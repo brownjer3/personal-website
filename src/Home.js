63df145f0f6af579f920f4dae9e736d6ef2884ba
jerry <brownjer3@gmail.com>
2021-05-12 13:18:27 -0700

    add noreferrer to home links

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
                         <Image src='https://media-exp1.licdn.com/dms/image/C4D03AQHqlCtHTbbgsQ/profile-displayphoto-shrink_800_800/0/1542059961980?e=1626307200&v=beta&t=iv9ieOy6J10NdtRVdVNJHlbcbLk-8qfSnhD-jL8X8uo' roundedCircle fluid className='border'/>
                     </div>
                     <p>
-                    I'm a Software Engineer currently based in Southern California. I like to build highly usable software with a focus on UX/UI. Over the past 7 years, I've worked as a Product Manager for <a href='https://www.stickk.com/' target="_blank">stickK</a> and a Technical Support Specialist for <a href='https://meetup.com/' target="_blank">Meetup</a>.
+                    I'm a Software Engineer currently based in Southern California. I like to build highly usable software with a focus on UX/UI. Over the past 7 years, I've worked as a Product Manager for <a href='https://www.stickk.com/' target="_blank" rel="noreferrer">stickK</a> and a Technical Support Specialist for <a href='https://meetup.com/' target="_blank" rel="noreferrer">Meetup</a>.
                     </p>
                 </Jumbotron>
                 <TechBanner />
