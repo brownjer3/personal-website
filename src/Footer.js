861065909d3ac45339f5a818cc625d39f4a29333
jerry <brownjer3@gmail.com>
2021-05-12 13:17:51 -0700

    add noreferrer to footer links

--- src/Footer.js
+++ src/Footer.js
@@ -7,10 +7,10 @@ export default function Footer() {
     return(
         <Container className='d-flex justify-content-center mt-3'>
             <Row className="w-50 justify-content-around my-3" >
-                <a href='https://medium.com/@brownjer3' target='_blank'><FontAwesomeIcon icon={faMedium} className='text-white' size="2x"/></a>
-                <a href='https://github.com/brownjer3' target='_blank'><FontAwesomeIcon icon={faGithub} className='text-white' size="2x"/></a>
-                <a href='https://www.linkedin.com/in/jerry-brown-/' target='_blank'><FontAwesomeIcon icon={faLinkedin} className='text-white' size="2x"/></a>
-                <a href="https://www.youtube.com/channel/UCUN7UOWbXjyXGHkVNcLGyCA" target='_blank'><FontAwesomeIcon icon={faYoutube} className='text-white' size="2x"/></a>
+                <a href='https://medium.com/@brownjer3' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faMedium} className='text-white' size="2x"/></a>
+                <a href='https://github.com/brownjer3' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faGithub} className='text-white' size="2x"/></a>
+                <a href='https://www.linkedin.com/in/jerry-brown-/' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} className='text-white' size="2x"/></a>
+                <a href="https://www.youtube.com/channel/UCUN7UOWbXjyXGHkVNcLGyCA" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faYoutube} className='text-white' size="2x"/></a>
                 <a href="mailto:brownjer3@gmail.com"><FontAwesomeIcon icon={faEnvelope} className='text-white' size="2x"/></a>
             </Row>
         </Container>
