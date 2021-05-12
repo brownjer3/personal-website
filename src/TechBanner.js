f099accce2594e3b4e0926241e2750a09ca00bad
jerry <brownjer3@gmail.com>
2021-05-12 13:16:06 -0700

    remove icon animation tests

--- src/TechBanner.js
+++ src/TechBanner.js
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 import { techIcons } from './data/TechData'
 
 export default class TechBanner extends Component {
@@ -11,7 +10,7 @@ export default class TechBanner extends Component {
 
     render() {
         return(
-            <div className='border-top border-bottom border-success border-3 my-5'>
+            <div className='border-top border-bottom border-success my-5 techBanner'>
                     <Row className="d-flex justify-content-between align-items-center">
                         {this.makeIcons()}
                     </Row>
