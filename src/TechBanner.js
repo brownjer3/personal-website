1c659665e8e159c73a56f086345d66ea6a55add2
jerry <brownjer3@gmail.com>
2021-05-11 20:46:14 -0700

    add favicon

--- src/TechBanner.js
+++ src/TechBanner.js
@@ -2,18 +2,22 @@ import { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import { tech } from './data/TechData'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import IconJavascript from 'react-devicon/javascript/plain'
+import IconReact from 'react-devicon/react/original-wordmark';
 
 export default class TechBanner extends Component {
     makeIcons = () => {
-        return tech.map(t => <FontAwesomeIcon icon={t} />)
+        return tech.map(t => <t height={100} width={100} />)
     }
 
     render() {
         return(
             <Container >
                 <Row>
-                    {this.makeIcons()}
+                    {/* {this.makeIcons()} */}
+                    {/* <IconReact height={100} width={100} /> */}
+                    <img src={IconReact} />
                 </Row>
             </Container>
         )
--- src/data/TechData.js
+++ src/data/TechData.js
@@ -1,8 +1,20 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJs, faReact, faGit, faJira, faSketch, faHtml5, faCss3 } from '@fortawesome/free-brands-svg-icons'
+// import { faJs, faReact, faGit, faJira, faSketch, faHtml5, faCss3 } from '@fortawesome/free-brands-svg-icons'
+import IconJavascript from 'react-devicon/javascript/plain';
+import IconReact from 'react-devicon/react/original-wordmark';
+import IconRuby from 'react-devicon/ruby/plain-wordmark';
+import IconRails from 'react-devicon/rails/plain-wordmark';
+import IconTypescript from 'react-devicon/typescript/plain';
+import IconPostgresql from 'react-devicon/postgresql/plain-wordmark';
+import IconJquery from 'react-devicon/jquery/plain-wordmark';
+import IconPhotoshop from 'react-devicon/photoshop/line';
+import IconGit from 'react-devicon/git/plain-wordmark';
+import IconSketch from 'react-devicon/sketch/line-wordmark';
+import IconHtml5 from 'react-devicon/html5/plain-wordmark';
+import IconCss3 from 'react-devicon/css3/plain-wordmark';
+
+
 
 export const tech = [ 
-    faJs, faReact, faHtml5, faCss3, faGit, faJira, faSketch
+    IconHtml5, IconCss3, IconJavascript, IconReact, IconJquery, IconRuby, IconRails, IconTypescript, IconPostgresql, IconGit, IconSketch, IconPhotoshop
 ]
-
-//Redux, Ruby, Ruby on Rails, Sinatra, SQL, JQuery, Adobe Photoshop, Zendes
