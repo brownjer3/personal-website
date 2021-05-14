3d41be78feb6a24c4be6fc7227b7e5bf6d8ebc5b
jerry <brownjer3@gmail.com>
2021-05-14 16:28:02 -0700

    rename projects -> projectsContainer for clarity

--- src/mainComponents/Home.js
+++ src/mainComponents/Home.js
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import AboutMe from './AboutMe';
-import Projects from '../projects/Projects';
+import ProjectsContainer from '../projects/ProjectsContainer';
 import TechBanner from './TechBanner';
 
 
@@ -10,7 +10,7 @@ export default function Home() {
             <Container fluid>
                 <AboutMe />
                 <TechBanner />
-                <Projects />
+                <ProjectsContainer />
             </Container>
         </header>
     )
