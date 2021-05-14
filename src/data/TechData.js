367f0efd0af6d1b50ea90123481fbb656d085dd0
jerry <brownjer3@gmail.com>
2021-05-14 07:39:57 -0700

    edit tech icon list

--- src/data/TechData.js
+++ src/data/TechData.js
@@ -1,5 +1,7 @@
 export const techIcons = [
     "devicon-html5-plain-wordmark", "devicon-css3-plain-wordmark", "devicon-javascript-plain", "devicon-react-original-wordmark", "devicon-ruby-plain-wordmark", 
-    "devicon-rails-plain-wordmark", "devicon-jquery-plain-wordmark", "devicon-typescript-plain", "devicon-bootstrap-plain-wordmark", "devicon-git-plain-wordmark", 
+    "devicon-rails-plain-wordmark", "devicon-jquery-plain-wordmark", "devicon-bootstrap-plain-wordmark", "devicon-git-plain-wordmark", 
     "devicon-sketch-line-wordmark", "devicon-photoshop-plain"
 ]
+
+// "devicon-typescript-plain"
