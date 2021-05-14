64c24379e037e600178341bbbdd0e600dd4dcab5
jerry <brownjer3@gmail.com>
2021-05-14 15:35:12 -0700

    remove test border around project details

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -10,7 +10,7 @@ export default class ProjectCard extends Component {
 
     render(){
         return(
-            <Col className='project-card border my-3 text-reset'>
+            <Col className='project-card'>
                 <div class="overlay border border-success"></div>
                 <Image src={this.props.image} fluid rounded className='border project-img my-2'/>
                 <ProjectDetails {...this.props} />
