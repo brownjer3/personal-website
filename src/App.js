2349314c0df61e099999b923dd196d9cf401b669
jerry <brownjer3@gmail.com>
2022-09-23 11:18:45 -0700

    disable star background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Home from './mainComponents/Home'
-import Footer from './mainComponents/Footer'
-import Stars from './mainComponents/Stars';
+import "./App.css";
+import Home from "./mainComponents/Home";
+import Footer from "./mainComponents/Footer";
+import Stars from "./mainComponents/Stars";
 
 function App() {
-  return (
-      <div className="main">
-        <Stars />
-        <Home />
-        <Footer />
-      </div>
-  );
+	return (
+		<div className="main">
+			{/* <Stars /> */}
+			<Home />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
