f519ac1a39d7786b0da8fc70f2d1f2df86220ed0
jerry <brownjer3@gmail.com>
2022-09-23 14:31:50 -0700

    change background color to blue

--- src/mainComponents/Home.js
+++ src/mainComponents/Home.js
@@ -1,15 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import AboutMe from './AboutMe';
-import ProjectsContainer from '../projects/ProjectsContainer';
-import TechBanner from './TechBanner';
-
+import Container from "react-bootstrap/Container";
+import AboutMe from "./AboutMe";
+import ProjectsContainer from "../projects/ProjectsContainer";
+import TechBanner from "./TechBanner";
 
 export default function Home() {
-    return (
-        <Container fluid>
-            <AboutMe />
-            <TechBanner />
-            <ProjectsContainer />
-        </Container>
-    )
+	return (
+		<Container fluid>
+			<AboutMe />
+			<TechBanner />
+			<ProjectsContainer />
+		</Container>
+	);
 }
+
+// other things i've done
+// eagle scout
+// flatiron school
+// udemy solidity course certificate
+// grinnell soccer
+// grinnell swim
