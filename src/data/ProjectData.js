ccf15761d50fbb00e80d971d19f781b7868ac901
jerry <brownjer3@gmail.com>
2022-09-23 11:37:15 -0700

    generate dynamic button links for projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,43 +1,61 @@
-import nftImage from '../images/nft-cal-thumbnail.png';
-import cadenceImage from '../images/cadence-thumbnail.png';
-import projectImage from '../images/projectMode-thumbnail.png';
-import driveImage from '../images/drive-thumbnail.png';
+import nftImage from "../images/nft-cal-thumbnail.png";
+import cadenceImage from "../images/cadence-thumbnail.png";
+import projectImage from "../images/projectMode-thumbnail.png";
+import driveImage from "../images/drive-thumbnail.png";
+import dfvImage from "../images/dfv-thumbnail.png";
 
 export const projects = [
-    {
-        name: 'NFT Drop Caldendar',
-        desc: "A crowd-sourced calendar to track upcoming Non-Fungible Token releases",
-        tech: ['React', 'Redux', 'Rails'],
-        image: nftImage, 
-        github: "https://github.com/brownjer3/nft-drop-calendar", 
-        blog: "https://brownjer3.medium.com/gettin-nifty-with-react-js-80cb8615bc0", 
-        demo: "https://youtu.be/uMNPCzoiIww"
-    }, 
-    {
-        name: 'Cadence Tunes',
-        desc: "A playlist builder for runners that recommends songs with tempos that match a target running cadence and seed genres",
-        tech: ['Javascript', 'Rails'],
-        image: cadenceImage, 
-        github: "http://github.com/brownjer3/cadence-tunes-v2", 
-        blog: "https://medium.com/nerd-for-tech/running-playlist-builder-ft-dj-spotify-recs-cc96fd920d75", 
-        demo: "http://youtu.be/wRhMecMOHQw"
-    }, 
-    {
-        name: 'Flatiron Project Mode',
-        desc: "A social platform for Flatiron School students to share, discover, and engage with projects and their creators",
-        tech: ['Ruby', 'Rails'],
-        image: projectImage, 
-        github: "http://github.com/brownjer3/ProjectMode", 
-        blog: "https://brownjer3.medium.com/projectception-4e3001668d3a", 
-        demo: "http://youtu.be/JRpuGSE3o1U"
-    }, 
-    {
-        name: 'Drive My Car',
-        desc: "A platform to connect people who need their car relocated across a long distance with drivers willing to help",
-        tech: ['Ruby', 'Sinatra'],
-        image: driveImage, 
-        github: "https://github.com/brownjer3/drive-my-car", 
-        blog: "https://brownjer3.medium.com/location-location-location-b6ed31ede71e", 
-        demo: "https://www.youtube.com/watch?v=biFK6HEu2kg"
-    }
-]
+	{
+		name: "Deep Fungible Value",
+		desc: "A live web application for users to browse and analyze the rarity of NFT’s on GameStop's NFT Marketplace",
+		tech: ["React", "Redux"],
+		image: dfvImage,
+		links: {
+			website: "http://deepfungiblevalue.io/",
+		},
+	},
+	{
+		name: "NFT Drop Caldendar",
+		desc: "A crowd-sourced calendar to track upcoming Non-Fungible Token releases",
+		tech: ["React", "Redux", "Rails"],
+		image: nftImage,
+		links: {
+			github: "https://github.com/brownjer3/nft-drop-calendar",
+			blog: "https://brownjer3.medium.com/gettin-nifty-with-react-js-80cb8615bc0",
+			demo: "https://youtu.be/uMNPCzoiIww",
+		},
+	},
+	{
+		name: "Cadence Tunes",
+		desc: "A playlist builder for runners that recommends songs with tempos that match a target running cadence and seed genres",
+		tech: ["Javascript", "Rails"],
+		image: cadenceImage,
+		links: {
+			github: "http://github.com/brownjer3/cadence-tunes-v2",
+			blog: "https://medium.com/nerd-for-tech/running-playlist-builder-ft-dj-spotify-recs-cc96fd920d75",
+			demo: "http://youtu.be/wRhMecMOHQw",
+		},
+	},
+	{
+		name: "Flatiron Project Mode",
+		desc: "A social platform for Flatiron School students to share, discover, and engage with projects and their creators",
+		tech: ["Ruby", "Rails"],
+		image: projectImage,
+		links: {
+			github: "http://github.com/brownjer3/ProjectMode",
+			blog: "https://brownjer3.medium.com/projectception-4e3001668d3a",
+			demo: "http://youtu.be/JRpuGSE3o1U",
+		},
+	},
+	{
+		name: "Drive My Car",
+		desc: "A platform to connect people who need their car relocated across a long distance with drivers willing to help",
+		tech: ["Ruby", "Sinatra"],
+		image: driveImage,
+		links: {
+			github: "https://github.com/brownjer3/drive-my-car",
+			blog: "https://brownjer3.medium.com/location-location-location-b6ed31ede71e",
+			demo: "https://www.youtube.com/watch?v=biFK6HEu2kg",
+		},
+	},
+];
--- src/projects/ProjectDetails.js
+++ src/projects/ProjectDetails.js
@@ -1,15 +1,33 @@
-import { Row, Col, Button, Container } from 'react-bootstrap';
+import { Row, Button, Container } from "react-bootstrap";
 
-export default function ProjectDetails({...props}) {
-    return(
-        <Container fluid className="project-details d-flex flex-column justify-content-around">
-            <h3 className="text-responsive">{props.name}</h3>
-            <p className="text-responsive">{props.desc}</p>
-            <Row className="d-flex justify-content-around">
-                <Button variant='success' href={props.blog} target='_blank' size='sm'>Read blog post</Button>
-                <Button variant='success' href={props.github} target='_blank' size='sm'>View source code</Button>
-                <Button variant='success' href={props.demo} target='_blank' size='sm'>Watch demo</Button>
-            </Row>
-        </Container>
-    )
+export default function ProjectDetails({ ...props }) {
+	const buttonLabels = {
+		website: "Visit website",
+		blog: "Read blog post",
+		github: "View source code",
+		demo: "Watch demo",
+	};
+
+	const makeButtons = (links) => {
+		return Object.keys(links).map((link) => {
+			return (
+				<Button variant="success" href={links[link]} target="_blank" size="sm">
+					{buttonLabels[link]}
+				</Button>
+			);
+		});
+	};
+
+	return (
+		<Container
+			fluid
+			className="project-details d-flex flex-column justify-content-around"
+		>
+			<h3 className="text-responsive">{props.name}</h3>
+			<p className="text-responsive">{props.desc}</p>
+			<Row className="d-flex justify-content-around">
+				{makeButtons(props.links)}
+			</Row>
+		</Container>
+	);
 }
