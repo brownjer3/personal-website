91a96ff162a8a8468554c23566440bdcc105125f
jerry <brownjer3@gmail.com>
2022-09-27 13:41:34 -0700

    add full navbar structure and styling

--- src/mainComponents/Navbar.js
+++ src/mainComponents/Navbar.js
@@ -3,13 +3,32 @@
 const Navbar = () => {
 	return (
 		<div className="top-nav">
-			{/* <div>1</div>
-			<div>2</div> */}
-			<div className="resume">
-				<a href="#" className="resume-btn">
-					Resume
+			<div className="logo">[LOGO]</div>
+			<div className="navbar-link">
+				<span className="navbar-number">01.</span>{" "}
+				<a href="#about" className="navbar-text">
+					About
 				</a>
 			</div>
+			<div className="navbar-link">
+				<span className="navbar-number">02.</span>{" "}
+				<a href="#experience" className="navbar-text">
+					Experience
+				</a>
+			</div>
+			<div className="navbar-link">
+				<span className="navbar-number">03.</span>{" "}
+				<a href="#work" className="navbar-text">
+					Work
+				</a>
+			</div>
+			<div className="navbar-link">
+				<span className="navbar-number">04.</span>{" "}
+				<a href="#misc" className="navbar-text">
+					Misc
+				</a>
+			</div>
+			<div className="resume-box">Resume</div>
 		</div>
 	);
 };
