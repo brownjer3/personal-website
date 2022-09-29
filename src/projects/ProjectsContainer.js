11720f18cf324bc690e9f18d55a740dcef0b2fae
jerry <brownjer3@gmail.com>
2022-09-29 14:07:29 -0700

    start to restructure projects section

--- src/projects/ProjectsContainer.js
+++ src/projects/ProjectsContainer.js
@@ -1,24 +1,93 @@
 import { Component } from "react";
-import { Container, Row } from "react-bootstrap";
 import { projects } from "../data/ProjectData";
 import ProjectCard from "./ProjectCard";
 
 export default class Projects extends Component {
-	makeProjects = () => {
-		return projects.map((project) => {
-			return <ProjectCard {...project} />;
-		});
-	};
+	// makeProjects = () => {
+	// 	return projects.map((project) => {
+	// 		return <ProjectCard {...project} />;
+	// 	});
+	// };
 
 	render() {
 		return (
-			<Container fluid id="work">
-				<h2 className="mb-5 text-left">Some Things I've Built</h2>
-				<Row className="projects" xs={1} lg={2} noGutters>
-					{this.makeProjects()}
-				</Row>
-				{/* <h2>Other Noteworthy Projects</h2> */}
-			</Container>
+			<section id="work" className="border">
+				<h2 className="section-header">
+					<span className="blue-text">03. </span>Some Things I've Built
+				</h2>
+				<div className="projects-container">
+					<img src={projects[0].image} className="border project-image" />
+					<div className="project-content">
+						<h4>Deep Fungible Value</h4>
+						<p className="content-text">{projects[0].desc}</p>
+						<ul className="content-text">
+							<li>React</li>
+							<li>Redux</li>
+							<li>Bootstrap</li>
+						</ul>
+						<ul className="content-text">
+							<li>Link 1</li>
+							<li>Link 2</li>
+						</ul>
+					</div>
+				</div>
+				<div className="projects-container">
+					<div className="project-content text-left">
+						<h4>NFT Drop Calendar</h4>
+						<p className="content-text">{projects[1].desc}</p>
+						<ul className="content-text">
+							<li>React</li>
+							<li>Redux</li>
+							<li>Bootstrap</li>
+						</ul>
+						<ul className="content-text">
+							<li>Link 1</li>
+							<li>Link 2</li>
+						</ul>
+					</div>
+					<img src={projects[1].image} className="border project-image" />
+				</div>
+				<div className="projects-container">
+					<img src={projects[2].image} className="border project-image" />
+					<div className="project-content">
+						<h4>Cadence Tunes</h4>
+						<p className="content-text">{projects[2].desc}</p>
+						<ul className="content-text">
+							<li>Javascript</li>
+							<li>Spotify API</li>
+						</ul>
+						<ul className="content-text">
+							<li>Link 1</li>
+							<li>Link 2</li>
+						</ul>
+					</div>
+				</div>
+			</section>
 		);
 	}
 }
+
+// <section id="experience" className="border">
+// 	<h2 className="section-header">
+// 		<span className="blue-text">02. </span>Where I've Worked
+// 	</h2>
+// 	<div className="experience-container">
+// 		<div className="company-list">
+// 			<li>stickK</li>
+// 			<li>Meetup</li>
+// 		</div>
+// 		<div className="job-content">
+// 			<h4>Product Manager @ stickK</h4>
+// 			<p className="content-text">2017 - present</p>
+// 			<ul className="content-text">
+// 				<li>bullet point #1</li>
+// 				<li>bullet point #2</li>
+// 				<li>bullet point #3</li>
+// 			</ul>
+// 		</div>
+// 	</div>
+// </section>;
+
+// <Row className="projects" xs={1} lg={2} noGutters>
+// 	{this.makeProjects()}
+// </Row>;
