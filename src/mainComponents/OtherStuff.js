d06dbbb5dec305c8aed5710f6025c8446ddbe834
jerry <brownjer3@gmail.com>
2022-09-29 14:28:14 -0700

    add basic structure for misc section

--- src/mainComponents/OtherStuff.js
+++ src/mainComponents/OtherStuff.js
@@ -1,16 +1,36 @@
-import Container from "react-bootstrap/Container";
-
 const OtherStuff = () => {
 	return (
-		<Container fluid className="my-4 text-left" id="misc">
-			<h2 className="mb-5">Misc Extras</h2>
-			<li>2008: Eagle Scout</li>
-			<li>2013: Grinnell College Men's Swimming Record Holder</li>
-			<li>2014: Grinnell College Men's Soccer Captain</li>
-			<li>2021: Flatiron School Full-Stack Engineering Bootcamp</li>
-			<li>2021: Ironman Madison Finisher</li>
-			<li>2022: Udemy Solidity Course Certificate</li>
-		</Container>
+		<section id="misc">
+			<h2 className="section-header">
+				<span className="blue-text">04. </span>Misc Extras
+			</h2>
+			<div className="misc-container">
+				<div className="misc-item border">
+					Udemy Solidity Course Certificate
+					<p className="content-text">2022</p>
+				</div>
+				<div className="misc-item border">
+					Ironman Madison Finisher<p className="content-text">2021</p>
+				</div>
+				<div className="misc-item border">
+					Flatiron School Full-Stack Engineering Bootcamp
+					<p className="content-text">2021</p>
+				</div>
+				<div className="misc-item border">
+					Grinnell College Men's Soccer Captain
+					<p className="content-text">2014</p>
+				</div>
+				<div className="misc-item border">
+					Grinnell College Men's Swimming Record Holder
+					<p className="content-text">2013</p>
+				</div>
+
+				<div className="misc-item border">
+					Eagle Scout
+					<p className="content-text">2008</p>
+				</div>
+			</div>
+		</section>
 	);
 };
 
