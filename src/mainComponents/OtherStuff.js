5af9cfc815af20e32847846e187bc4c8eac9d319
jerry <brownjer3@gmail.com>
2022-09-29 14:49:54 -0700

    add hover effect to misc cards

--- src/mainComponents/OtherStuff.js
+++ src/mainComponents/OtherStuff.js
@@ -5,27 +5,27 @@ const OtherStuff = () => {
 				<span className="blue-text">04. </span>Misc
 			</h2>
 			<div className="misc-container">
-				<div className="misc-item border">
+				<div className="misc-item">
 					Udemy Solidity Course Certificate
 					<p className="content-text">2022</p>
 				</div>
-				<div className="misc-item border">
+				<div className="misc-item">
 					Ironman Madison Finisher<p className="content-text">2021</p>
 				</div>
-				<div className="misc-item border">
+				<div className="misc-item">
 					Flatiron School Full-Stack Engineering Bootcamp
 					<p className="content-text">2021</p>
 				</div>
-				<div className="misc-item border">
+				<div className="misc-item">
 					Grinnell College Men's Soccer Captain
 					<p className="content-text">2014</p>
 				</div>
-				<div className="misc-item border">
+				<div className="misc-item">
 					Grinnell College Men's Swimming Record Holder
 					<p className="content-text">2013</p>
 				</div>
 
-				<div className="misc-item border">
+				<div className="misc-item">
 					Eagle Scout
 					<p className="content-text">2008</p>
 				</div>
