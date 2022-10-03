d91b3dfc30d3d33954d1b50a5fdcc4f04d7ce627
jerry <brownjer3@gmail.com>
2022-10-03 09:35:37 -0700

    reformat intro description text

--- src/mainComponents/Intro.js
+++ src/mainComponents/Intro.js
@@ -4,7 +4,7 @@ const Intro = () => {
 			<p className="blue-text">Hi there! My name is</p>
 			<h1 className="primary-text">Jerry Safter-Brown.</h1>
 			<h1 className="secondary-text">I'm a Frontend Software Engineer.</h1>
-			<p className="secondary-text">
+			<p className="intro-description">
 				I'm a Software Engineer currently based in Southern California. I like
 				building React applications that prioritize speed and user experience,
 				and I'm always looking to learn something new.
