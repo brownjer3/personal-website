464790c026ec9d8e92271cf2bf93d5d3f44202ee
jerry <brownjer3@gmail.com>
2022-10-05 17:56:41 -0400

    add and modify content for misc section

--- src/data/MiscData.js
+++ src/data/MiscData.js
@@ -37,33 +37,33 @@ export const miscData = [
 		title: "Ironman Madison Finisher",
 		organization: "Ironman Foundation",
 		year: "2021",
-		desc: "Raised $3,500 for the Ironman Foundation and completed the course with a time of 14:14:14",
+		desc: "Raised $3,500 for the Ironman Foundation and completed the course with a time of 15:22:01.",
 		icon: faRunning,
 	},
 	{
-		title: "Full-Stack Engineering Bootcamp",
+		title: "Full-Stack Engineering Bootcamp Grad",
 		organization: "Flatiron School",
 		year: "2021",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		desc: "At Flatiron, I completed 5 full-stack engineering modules learning HTML, CSS, Ruby, Ruby on Rails, JavaScript, and React.",
 		icon: faCode,
 	},
 	{
-		title: "Men's Soccer Captain",
-		organization: "Grinnell College",
+		title: "Men's Team Captain",
+		organization: "Grinnell College Soccer",
 		year: "2014",
-		desc: "My Senior year, I was elected team captain by my teammates.",
+		desc: "My Senior year, 20+ teammates elected me as captain where I functioned as a liaison between the team and coaching staff.",
 		icon: faFutbol,
 	},
 	{
-		title: "Men's Soccer Academic All-Conference Player",
-		organization: "Grinnell College",
+		title: "Academic All-Conference Player",
+		organization: "Grinnell College Soccer",
 		year: "2014",
-		desc: "My Senior year, I was elected team captain by my teammates.",
+		desc: "My Senior year, I was awared Academic All-Conference for maintaining a certain GPA while involved in a varisty sport.",
 		icon: faAward,
 	},
 	{
-		title: "Men's Swimming Record Holder",
-		organization: "Grinnell College",
+		title: "Men's 400-Freestyle Record Holder",
+		organization: "Grinnell College Swimming",
 		year: "2013",
 		desc: "My Junior year, I was the first leg of the 400 yard freestyle relay that set a new school record with a time of 3:06.74.",
 		icon: faSwimmer,
@@ -72,7 +72,11 @@ export const miscData = [
 		title: "Eagle Scout",
 		organization: "Boy Scouts of America",
 		year: "2008",
-		desc: "Over the course of 10 years, I rose through the Boy Scout ranks to acheive Eagle Scout.",
+		desc: "Over the course of 10+ years, I earned over 25 merit badges, acted in various leadership roles, and completed an Eagle Service Project, ultimately achieving the rank of Eagle.",
 		icon: faCampground,
 	},
 ];
+
+// skill learning
+// volunteer work
+// active member
