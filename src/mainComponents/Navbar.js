bdbe23c12d99608121208d6d177072727a06bf23
Jerry Brown <jerrybrown@Jerrys-MBP.lan>
2022-10-17 11:35:14 -0700

    add updated resume

--- src/mainComponents/Navbar.js
+++ src/mainComponents/Navbar.js
@@ -1,4 +1,4 @@
-import Resume from "../data/JB_Temp_Resume.pdf";
+import Resume from "../data/JSB-Resume-2022.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
