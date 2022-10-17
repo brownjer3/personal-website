93f929056354de657ab9dc0881c177261760c46f
Jerry Brown <jerrybrown@Jerrys-MBP.lan>
2022-10-17 11:25:22 -0700

    fix navbar link colors

--- src/mainComponents/Intro.js
+++ src/mainComponents/Intro.js
@@ -1,9 +1,9 @@
 const Intro = () => {
 	return (
 		<section className="text-left">
-			<p className="hi-text">Hi there! My name is:</p>
+			<p className="hi-text">Hi there! My name is</p>
 			<h1 className="intro-name">Jerry Safter-Brown.</h1>
-			<h1 className="intro-title">I'm a Frontend Software Engineer.</h1>
+			<h1 className="intro-title">I'm a Front-End Software Engineer.</h1>
 			<p className="intro-description">
 				I like designing, building, and shipping web applications that
 				prioritize speed and user experience. Currently, I'm focused on creating
