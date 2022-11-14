12a7a59b456ec33af530e418d1659c8ff613316d
Jerry Brown <brownjer3@gmail.com>
2022-11-14 11:08:09 -0800

    update title in intro section

--- src/mainComponents/Intro.js
+++ src/mainComponents/Intro.js
@@ -3,7 +3,7 @@ const Intro = () => {
 		<section className="text-left" id="intro">
 			<p className="hi-text">Hi there! My name is</p>
 			<h1 className="intro-name">Jerry Safter-Brown.</h1>
-			<h1 className="intro-title">I'm a Front-End Software Engineer.</h1>
+			<h1 className="intro-title">I'm a Full-Stack Software Engineer.</h1>
 			<p className="intro-description">
 				I like designing, building, and shipping web applications that
 				prioritize speed and user experience. Currently, I'm focused on creating
